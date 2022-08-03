ed89be8f9e7ade625b8147e1eb0bb4d7d4a67c12
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2022-08-03 09:23:57 +0500

    Completed SheetView sample

--- Food/Controllers/DishesListViewController.swift
+++ Food/Controllers/DishesListViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class DishesListViewController: UIViewController {
 
-    var startDrag: CGFloat = 0
     var scrollViewContentOffset: CGFloat = 0
     var padding: CGFloat = 0
     var dishesList: Array<Dish> = DishCoder.getDishes()
@@ -20,8 +19,7 @@ class DishesListViewController: UIViewController {
     
     var collectionViewMinHeight: CGFloat = 400
     var collectionViewMaxHeight: CGFloat = 800
-    var maxTotalTranslation: CGFloat = 800
-    var minTotalTranslation: CGFloat = 400
+    var totalTranslation: CGFloat = 0
     var gestureFlag: Bool = true
     
     override func viewDidLoad() {
@@ -29,10 +27,8 @@ class DishesListViewController: UIViewController {
         padding = view.bounds.width / 20
 
         collectionViewMinHeight = view.bounds.height * 0.3
-        startDrag = collectionViewMinHeight
         collectionViewMaxHeight = view.bounds.height * 0.6
-        minTotalTranslation = collectionViewMinHeight
-        maxTotalTranslation = collectionViewMaxHeight
+        totalTranslation = collectionViewMinHeight
         
         let layout = UICollectionViewFlowLayout()
         collectionView.collectionViewLayout = layout
@@ -54,77 +50,43 @@ class DishesListViewController: UIViewController {
         return (verticalLimit * (1 - log10(yPosition/verticalLimit)))
     }
     
-    enum Zone {
-        case middle
-        case top
-        case bottom
-    }
-    
-    var zones: Zone = .middle
-    
     var drag: CGFloat = .zero
     @IBAction func dragGesture(_ gesture: UIPanGestureRecognizer) {
         
+        let translation = gesture.translation(in: view).y
+        
         if  gesture.state == .began {
-            //drag = collectionViewMinHeight
+            drag = .zero
         }
         if gesture.state == .changed {
-            drag -= gesture.translation(in: view).y
-            print("GestureTran: \(gesture.translation(in: view).y)")
-            print("drag (x): \(drag)")
-            print("viewHeight: \(view.bounds.height)")
-            collectionViewHeight.constant = collectionViewMinHeight + (1.0 - (1.0 / ((drag * 0.55 / view.bounds.height) + 1.0))) * view.bounds.height
-            print("function: \((1.0 - (1.0 / ((drag * 0.55 / view.bounds.height) + 1.0))) * view.bounds.height)")
-            print("constraint: \(collectionViewHeight.constant)")
-        }
-        if gesture.state == .ended {
-
-        }
-
-//        collectionViewHeight.constant -= gesture.translation(in: view).y
-//
+            
+            if collectionViewHeight.constant <= collectionViewMaxHeight && collectionViewHeight.constant >= collectionViewMinHeight {
+                collectionViewHeight.constant -= translation
+                
+                totalTranslation = collectionViewHeight.constant
+                drag = totalTranslation
+            } else if collectionViewHeight.constant < collectionViewMinHeight {
+                drag -= translation
+                collectionViewHeight.constant = totalTranslation + (1.0 - (1.0 / (((drag - totalTranslation) * 0.3 / view.bounds.height) + 1.0))) * view.bounds.height
+            } else if collectionViewHeight.constant > collectionViewMaxHeight {
+                drag -= translation
+                collectionViewHeight.constant = totalTranslation + (1.0 - (1.0 / (((drag - totalTranslation) * 0.3 / view.bounds.height) + 1.0))) * view.bounds.height
+            }
+            
+            
 //            drag -= gesture.translation(in: view).y
-//            collectionViewHeight.constant = (1.0 – (1.0 / ((x * c / d) + 1.0))) * d
+//            print("GestureTran: \(gesture.translation(in: view).y)")
+//            print("drag (x): \(drag)")
+//            print("viewHeight: \(view.bounds.height)")
+//            collectionViewHeight.constant = collectionViewMinHeight + (1.0 - (1.0 / ((drag * 0.55 / view.bounds.height) + 1.0))) * view.bounds.height
+//            print("function: \((1.0 - (1.0 / ((drag * 0.55 / view.bounds.height) + 1.0))) * view.bounds.height)")
+//            print("constraint: \(collectionViewHeight.constant)")
+        }
         
-       // startDrag = translation
         gesture.setTranslation(.zero, in: view)
     }
     
-//    let translation = gesture.translation(in: view).y
-//    if collectionViewHeight.constant <= collectionViewMinHeight {
-//        if translation < 0 {
-//            collectionViewHeight.constant -= translation
-//        }
-//        minTotalTranslation += translation
-//        collectionViewHeight.constant = logMinConstraintValueForYPosition(yPosition: minTotalTranslation, verticalLimit: collectionViewMinHeight)
-//        endedGestureState(gesture: gesture)
-//
-//    } else if collectionViewHeight.constant >= collectionViewMaxHeight {
-//        if gesture.view is UICollectionView {
-//            if translation > 0 && gestureFlag == true {
-//                collectionViewHeight.constant -= translation
-//            }
-//        } else {
-//            maxTotalTranslation -= translation
-//            collectionViewHeight.constant = logMaxConstraintValueForYPosition(yPosition: maxTotalTranslation, verticalLimit: collectionViewMaxHeight)
-//
-//            endedGestureState(gesture: gesture)
-//        }
-//    } else {
-//
-//        if collectionViewHeight.constant - translation > collectionViewMaxHeight {
-//            collectionViewHeight.constant = collectionViewMaxHeight
-//        } else if collectionViewHeight.constant - translation < collectionViewMinHeight {
-//            collectionViewHeight.constant = collectionViewMinHeight
-//        } else {
-//            collectionViewHeight.constant -= translation
-//        }
-//        endedGestureState(gesture: gesture)
-//    }
-//
-//
-//    gesture.setTranslation(.zero, in: view)
-    
+
     //helper
     
     func endedGestureState(gesture: UIPanGestureRecognizer){
@@ -140,8 +102,6 @@ class DishesListViewController: UIViewController {
     
     func animateHeightTo(_ limitHeight: CGFloat) {
         collectionViewHeight.constant = limitHeight
-        minTotalTranslation = collectionViewMinHeight
-        maxTotalTranslation = collectionViewMaxHeight
         
         UIView.animate(withDuration: 0.1, delay: 0, options: .allowUserInteraction, animations: {
             self.view.layoutIfNeeded()
